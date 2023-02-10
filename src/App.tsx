e92071fb15506b592c4a5242f6522daecd0db27c
Mateo Presa Castro <mateopresacastro@gmail.com>
2023-02-10 08:21:59 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Main from './components/Main';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="h-screen w-screen">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main.tsx
@@ -0,0 +1,4 @@
+const Main = () => {
+  return null;
+};
+export default Main;
