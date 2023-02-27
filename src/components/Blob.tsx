78d1aa112ccd1208c0d34f447165e35282dfd9cf
Mateo Presa Castro <mateopresacastro@gmail.com>
2023-02-27 16:34:24 +0100

    chore: comment

--- src/components/Blob.tsx
+++ src/components/Blob.tsx
@@ -10,7 +10,7 @@ const Blob = () => {
       const { clientX, clientY } = event;
       myRef.current.animate(
         {
-          // -64 to center mouse cursor in the middle of the blob, which is 224px (h-56 in tailwind)
+          // -112 to center mouse cursor in the middle of the blob, which is 224px (h-56 in tailwind)
           left: `${clientX - 112}px`,
           top: `${clientY - 112}px`,
         },
