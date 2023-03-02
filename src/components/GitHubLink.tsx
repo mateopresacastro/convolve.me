d590032eb3d8575ce693504664c9631f917800bf
Mateo Presa Castro <mateopresacastro@gmail.com>
2023-03-02 12:36:25 +0100

    acc: aria-label on <a>

--- src/components/GitHubLink.tsx
+++ src/components/GitHubLink.tsx
@@ -3,7 +3,10 @@ import { RxGithubLogo } from 'react-icons/rx';
 const GitHubLink = () => {
   return (
     <footer className="absolute bottom-5">
-      <a href="https://github.com/MateoPresaCastro/convolve.me">
+      <a
+        href="https://github.com/MateoPresaCastro/convolve.me"
+        aria-label="Github"
+      >
         <RxGithubLogo className="h-5 w-5 cursor-pointer text-zinc-400 transition duration-300 ease-in-out hover:text-zinc-700" />
       </a>
     </footer>
