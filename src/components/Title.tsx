0e3a86ea39c5c59ff14ddd3ce2854b1621e60604
Mateo Presa Castro <mateopresacastro@gmail.com>
2023-03-03 16:34:59 +0100

    fix: margin

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,6 +1,6 @@
 const Title = () => {
   return (
-    <section className="mb-10 flex flex-col items-center justify-center">
+    <section className="mb-20 flex flex-col items-center justify-center">
       <h1 className="mb-3 text-5xl font-extrabold text-zinc-700">
         convolve<span className="font-light">.me</span>
       </h1>
