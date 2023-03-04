59791be711bae56e999feb2fd39b8854ec2de9eb
Mateo Presa Castro <mateopresacastro@gmail.com>
2023-03-04 09:40:14 +0100

    style: center

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,12 +1,14 @@
 export default function Title() {
   return (
-    <section className="mb-20 flex flex-col items-center justify-center">
-      <h1 className="mb-3 text-5xl font-extrabold text-zinc-700">
-        convolve<span className="font-light">.me</span>
-      </h1>
-      <p className="text-sm text-zinc-400">
-        Upload or record two samples and press start ✨
-      </p>
-    </section>
+    <div className="absolute top-40">
+      <section className=" mb-20 flex flex-col items-center justify-center">
+        <h1 className="mb-3 text-5xl font-extrabold text-zinc-700">
+          convolve<span className="font-light">.me</span>
+        </h1>
+        <p className="text-sm text-zinc-400">
+          Upload or record two samples and press start ✨
+        </p>
+      </section>
+    </div>
   );
 }
